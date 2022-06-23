b4d80d3602f17f0f0e232760431478ce6123acd6
roseline_dosu <dosuroseline@gmail.com>
2022-06-24 00:21:57 +0100

    second

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('name').innerHTML="Name: Dosu Roseline";
-document.getElementById('height').innerHTML="Height: " + 1.57;
-document.getElementById('country').innerHTML="Country: Nigeria";
-
-
-alert("Name: Dosu Roseline \n Height: 1.57 \n Country: Nigeria")
