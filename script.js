58bf4db3d30608a27629f1da420ffe94957f1d0c
roseline_dosu <dosuroseline@gmail.com>
2022-06-24 00:07:34 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('name').innerHTML="Name: Dosu Roseline";
+document.getElementById('height').innerHTML="Height: " + 1.57;
+document.getElementById('country').innerHTML="Country: Nigeria";
+
+
+alert("Name: Dosu Roseline \n Height: 1.57 \n Country: Nigeria")
